3a663d3d3ae9784e3d526d17f54693040c23213a
Cristian_Gomez26 <porte20k7@gmail.com>
2022-07-10 17:03:34 -0300

    mi primer repo JS

---
+++ código.js
@@ -0,0 +1,10 @@
+'use strict' //Modo estricto de JavaScript
+
+// Al cargarse el archivo JavaScript, se muestra el primer mensaje
+alert("Desea Continuar");
+
+// Al cargarse el archivo JavaScript, se muestra un segundo mensaje
+alert("Soy El Primer Script");
+
+//imprimir por consola
+console.log("Hola como estas???")
